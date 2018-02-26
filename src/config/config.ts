dc65d3f519ba93400e350d2d2acce20b7148cc57
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-02-26 14:13:29 +0100

    changed config structure to adapt to deploy structure from contracts repoo

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
-import { abi } from './charge';
+import * as ChargingStationConfiguration from './ChargingStation.json';
 
 export const config = {
     version: '0.1.0',
     node: 'ws://localhost:8546',
-    chargeAbi: abi,
-    chargeAddr: '0x3588931e58506f063267aba0df50e4bafeb972df'
+    chargeAbi: ChargingStationConfiguration.abi,
+    chargeAddr: ChargingStationConfiguration.address
 };
