2fab34cb081d24ebeff8181f62169281cc456a95
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-02-27 09:36:45 +0100

    fixed getcoinbase issue

--- src/services/contract.ts
+++ src/services/contract.ts
@@ -1,11 +1,11 @@
 import Web3 = require('web3');
-import { Subject } from 'rxjs/Subject';
-import { config } from '../config/config';
-import { IContract } from '../models/contract';
-import { Request } from '../models/request';
-import { Connector } from '../models/connector';
-import { Receipt, ReturnStatusObject } from '../models/receipt';
-import { createPayload, createReceipt } from '../utils/helpers';
+import {Subject} from 'rxjs/Subject';
+import {config} from '../config/config';
+import {IContract} from '../models/contract';
+import {Request} from '../models/request';
+import {Connector} from '../models/connector';
+import {Receipt, ReturnStatusObject} from '../models/receipt';
+import {createPayload, createReceipt} from '../utils/helpers';
 
 export class Contract implements IContract {
 
@@ -42,6 +42,10 @@ export class Contract implements IContract {
         });
     }
 
+    async getCoinbase() {
+        return await this.web3.eth.getCoinbase();
+    }
+
     async queryState(method: string, ...args: any[]): Promise<any> {
         const query = this.contract.methods[method](...args);
         return query.call();
@@ -52,9 +56,9 @@ export class Contract implements IContract {
         // console.log('args:', args);
         const tx = this.contract.methods[method](...args);
         // const gas1 = await this.web3.eth.estimateGas({ data: tx.rawTransaction, from: coinbase });
-        const gas2 = await tx.estimateGas({ from: coinbase });
+        const gas2 = await tx.estimateGas({from: coinbase});
         await this.personal.unlockAccount(coinbase, this.pass, 30);
-        const receipt = await tx.send({ from: coinbase, gas: gas2 });
+        const receipt = await tx.send({from: coinbase, gas: gas2});
         return createReceipt(receipt);
     }
 
