101a5fff7b38759e43e625f0619674fe16991e05
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-03-12 16:59:53 +0100

    adapted changes from contracts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,9 @@
-let ChargingStationConfiguration;
-
-try {
-    ChargingStationConfiguration = require('./ChargingStation.json');
-} catch (err) {
-    console.log('WARNING: No ChargingStation.json');
-    ChargingStationConfiguration = {
-        abi: [],
-        address: '0x547de1b5914a4b194df58f5818daabee76125e75'
-    };
-}
+import { ChargingStation } from "sharecharge-contracts";
 
 export const config = {
     version: '0.1.0',
     node: 'ws://localhost:8546',
-    chargeAbi: ChargingStationConfiguration.abi,
-    chargeAddr: ChargingStationConfiguration.address,
+    chargeAbi: ChargingStation.abi,
+    chargeAddr: ChargingStation.address,
     gasPrice: 18000000000
 };
