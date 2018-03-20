89dea8ab7947f1c6ffe69df5c62f5e02308e7d16
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-03-20 10:24:26 +0100

    updated package lock

--- src/utils/defsLoader.ts
+++ src/utils/defsLoader.ts
@@ -5,11 +5,15 @@ export const loadContractDefs = (stage) => {
 
     const localPath = path.join(__dirname, `../../node_modules/sharecharge-contracts/contract.defs.${stage}.json`);
 
+    console.log("local", localPath);
+
     if (fs.existsSync(localPath)) {
         return JSON.parse(fs.readFileSync(localPath, "utf8"));
     }
 
     let globalPath = process.env["HOME"] + `/.sharecharge/contract.defs.${stage}.json`;
+
+    console.log("global", globalPath);
 
     if (fs.existsSync(globalPath)) {
         return JSON.parse(fs.readFileSync(globalPath, "utf8"));
