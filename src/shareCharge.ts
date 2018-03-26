4c63707a2e5a931fbf9486e42a0fa0184a31fc6e
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-03-26 12:22:24 +0200

    cleanup, more types

--- src/shareCharge.ts
+++ src/shareCharge.ts
@@ -1,15 +1,11 @@
 import { ChargingService } from './services/chargingService';
 import { EvseService } from './services/evseService';
 import { StationService } from './services/stationService';
-import { Contract } from './models/contract';
 import { EventPoller } from './services/eventPoller';
 import { EventDispatcher } from './services/eventDispatcher';
-import { IDefs } from "./interfaces/iDefs";
-import { IServices } from "./interfaces/iServices";
-import { IConfig } from "./interfaces/iConfig";
 import { ConfigProvider } from './services/configProvider';
-import { IContractProvider, ContractProvider } from './services/contractProvider';
-import { Container, injectable, inject } from "inversify";
+import { ContractProvider } from './services/contractProvider';
+import { injectable, inject } from "inversify";
 import { Symbols } from './symbols';
 import "reflect-metadata";
 
@@ -24,9 +20,9 @@ export class ShareCharge {
     public readonly evses: EvseService;
     public readonly charging: ChargingService;
 
-    constructor(@inject(Symbols.StationSerivce) stationService?,
-                @inject(Symbols.EvseService) evseService?,
-                @inject(Symbols.ChargingService) chargingService?) {
+    constructor(@inject(Symbols.StationSerivce) stationService?: StationService,
+                @inject(Symbols.EvseService) evseService?: EvseService,
+                @inject(Symbols.ChargingService) chargingService?: ChargingService) {
         this.stations = stationService || new StationService(new ContractProvider(config));
         this.evses = evseService || new EvseService(new ContractProvider(config));
         this.charging = chargingService || new ChargingService(new ContractProvider(config));
