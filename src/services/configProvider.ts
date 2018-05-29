79d8a516da4e425276788c97f90412c88bfeb292
adamstaveley <adam@motionwerk.com>
2018-05-29 16:17:13 +0200

    more robust ipfsProvider in config

--- src/services/configProvider.ts
+++ src/services/configProvider.ts
@@ -2,7 +2,7 @@ export class ConfigProvider {
 
     public readonly stage: string;
     public readonly ethProvider: string;
-    public readonly ipfsProvider: string;
+    public readonly ipfsProvider: { host: string, port: string, protocol: string };
     public readonly gasPrice: number;
     public readonly pollingInterval: number;
     public tokenAddress: string;
@@ -10,7 +10,11 @@ export class ConfigProvider {
     constructor(config: any = {}) {
         this.stage = config.stage || 'local';
         this.ethProvider = config.ethProvider || 'http://localhost:8545';
-        this.ipfsProvider = config.ipfsProvider || '/ip4/127.0.0.1/tcp/5001';
+        this.ipfsProvider = config.ipfs || {
+            host: '127.0.0.1',
+            port: '5001',
+            protocol: 'http'
+        },
         this.gasPrice = config.gasPrice || 18000000000;
         this.pollingInterval = config.pollingInterval || 1000;
         this.tokenAddress = config.tokenAddress || "";
