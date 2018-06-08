1c124f9497d423168eb59d4394cbf7e2a153f9ac
adamstaveley <adam@motionwerk.com>
2018-06-08 21:52:11 +0200

    revert to 0.4.4

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -64,7 +64,7 @@ export class StorageService {
                 resolvedLocations.push({
                     scId,
                     data
-                })
+                });
             }
         }
 
